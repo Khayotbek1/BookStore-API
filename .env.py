ce2840d160418c508cc7e3927fefb02ef896e16b
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-07-18 00:41:23 +0500

    commit

---
+++ .env.py
@@ -0,0 +1,10 @@
+DEBUG=TRUE
+
+# Postgres konfiguratsiyasi
+DB_NAME=drf_db
+DB_USER=postgres
+DB_PASSWORD=0002
+DB_HOST=localhost
+DB_PORT=5432
+
+SECRET_KEY = django-insecure-9rwk^a8g-3sm0n811x-z(=ar2@j-rbsi+14+m)nkx8xkckhu)u
--- core/settings.py
+++ core/settings.py
@@ -104,8 +104,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore_db',
+        'USER': 'postgres',
+        'PASSWORD': '0002',
+        'PORT': '5432',
     }
 }
 
